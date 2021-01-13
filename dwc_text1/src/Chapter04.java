4fff9cedc38c0906211c90992291d88746054c53
西尾貴成 <takanari@nishiokiseinoMacBook-Pro.local>
2021-01-13 15:24:41 +0900

    chapter4 finished

---
+++ dwc_text1/src/Chapter04.java
@@ -0,0 +1,12 @@
+public class Chapter04 {
+    public static void main(String[] args) {
+        String name = "山田";
+        System.out.println(name);
+        name = "佐藤";
+        System.out.println(name);
+
+        final int luckyNumber = 7;
+        System.out.println(luckyNumber);
+
+    }
+}
